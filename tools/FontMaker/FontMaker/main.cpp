3d626b501192971c271c1a5cb6e6d14329e1e862
Tristan Dube <tristan.dube@gmail.com>
2016-02-06 20:44:16 -0500

    Font maker progress.

--- tools/FontMaker/FontMaker/main.cpp
+++ tools/FontMaker/FontMaker/main.cpp
@@ -7,6 +7,12 @@
 
 void ProcessFile(char* fileName);
 
+enum ProcessState
+{
+    SEEKING,
+
+};
+
 int main(int argc, char *argv[])
 {
     if (argc > 1)
@@ -31,5 +37,28 @@ int main(int argc, char *argv[])
 void ProcessFile(char* fileName)
 {
     SDL_Surface* font = IMG_Load(fileName);
+
+    if (font == NULL)
+    {
+        fprintf(stderr, "File not found or not correct image format.\n");
+        return;
+    }
+
+    Uint32* pix = (Uint32*)font->pixels;
+
+    for (int i = 0; i < font->w; i++)
+    {
+        for (int j = 0; j < font->h; j++)
+        {
+            Uint32 px = pix[i + (j * font->w)];
+
+            if (px == 0xFFFFFFFF)
+            {
+                int test = 5;
+            }
+
+            int test = 0;
+        }
+    }
 
 }
