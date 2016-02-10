d3fad0a597e809158fb6e7647291629410acf2ab
Tristan Dube <tristan.dube@gmail.com>
2016-02-10 12:24:26 -0500

    Added a new static class called FontProcessor that will allow scanning a font from any project.

---
+++ src/CommsFramework/FontProcessor.cpp
@@ -0,0 +1,185 @@
+#include "FontProcessor.h"
+
+#include <stdio.h>
+#include <string>
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "FRectangle.h"
+#include "SDLUtilities.h"
+#include "SDLTexture.h"
+
+FontProcessor::FontProcessor()
+{
+    CharacterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+}
+
+
+FontProcessor::~FontProcessor()
+{
+}
+
+BaseList<BaseTexture*>* FontProcessor::GetCharactersFromFont(BaseTexture* inTexture)
+{
+    SDLTexture* tex = dynamic_cast<SDLTexture*>(inTexture);
+
+    if (tex == NULL) // Texture needs to be in SDL Format
+        return;
+
+    BaseList<BaseTexture*>* characterTextures = new BaseList<BaseTexture*>();
+
+    SDL_Surface* font = tex->surface;
+
+    if (font == NULL)
+    {
+        fprintf(stderr, "File not found or not correct image format.\n");
+        return;
+    }
+
+    if (font->format->BitsPerPixel != 32)
+    {
+        fprintf(stderr, "Pixel format is not 32bit.\n");
+        return;
+    }
+
+    Uint32* pix = (Uint32*)font->pixels;
+
+    FRectangle* charRectangle;
+    int currentCharIndex = 0;
+
+    State = SEEKING;
+
+    int i = 0, j = 0;
+
+    int iters = 0;
+
+    while (true)
+    {
+        if (i >= font->w)
+        {
+            return;
+        }
+
+        Uint32 px = pix[i + (j * font->w)];
+
+        switch (State)
+        {
+            case SEEKING:
+            {
+                if (j >= font->h)
+                {
+                    i++;
+                    j = 0;
+                }
+                else
+                {
+                    if (px != BLANK)
+                    {
+                        State = BEGIN_CHAR;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+
+                break;
+            }
+            case BEGIN_CHAR:
+            {
+                charRectangle = new FRectangle();
+                charRectangle->Left = i;
+                charRectangle->Top = j;
+                charRectangle->Right = i;
+                charRectangle->Bottom = j;
+
+                State = PIXEL_FOUND;
+
+                break;
+            }
+            case LINE_SEEK:
+            {
+                if (j >= font->h)
+                {
+                    if (charRectangle->Right != i)
+                    {
+                        State = END_CHAR;
+                    }
+
+                    i++;
+                    j = 0;
+                }
+                else
+                {
+                    if (px != BLANK)
+                    {
+                        State = PIXEL_FOUND;
+                        break;
+                    }
+
+                    j++;
+                }
+
+                break;
+            }
+            case PIXEL_FOUND:
+            {
+                if (j >= font->h)
+                {
+                    State = LINE_SEEK;
+                    break;
+                }
+
+                if (px != BLANK)
+                {
+                    if (j < charRectangle->Top)
+                    {
+                        charRectangle->Top = j;
+                    }
+                    if (j > charRectangle->Bottom)
+                    {
+                        charRectangle->Bottom = j;
+                    }
+                    if (i > charRectangle->Right)
+                    {
+                        charRectangle->Right = i;
+                    }
+                }
+
+                j++;
+
+                break;
+            }
+            case END_CHAR:
+            {
+                SDL_Rect rec = FRectToSDL_Rect(*charRectangle);
+                rec.w += 1; // Have to add +1 to w and h to compensate the comversion from my rec to sdl rec. TODO : Problem in FRect impl ?
+                rec.h += 1;
+
+                SDL_Surface* subSurf = SDL_CreateRGBSurface(0, rec.w, rec.h, 32, font->format->Rmask, font->format->Gmask, font->format->Bmask, font->format->Amask);
+
+                SDL_BlitSurface(font, &rec, subSurf, NULL);
+
+                SDLTexture* characterTexture = new SDLTexture();
+                characterTexture->LoadFromSurface(subSurf);
+
+                characterTextures->Add(characterTexture);
+
+                currentCharIndex++;
+
+                State = SEEKING;
+
+                break;
+            }
+        }
+
+        iters++;
+    }
+
+    return characterTextures;
+}
+
+BaseList<SpriteFontGlyph*>* FontProcessor::GetCharacterDescriptors(BaseTexture * inTexture)
+{
+    return nullptr;
+}
---
+++ src/CommsFramework/FontProcessor.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class BaseTexture;
+class SpriteFontGlyph;
+
+#include "BaseList.h"
+
+#define BLANK 0x00FFFFFF
+
+class FontProcessor
+{
+public:
+    FontProcessor();
+    ~FontProcessor();
+
+    static char* CharacterSet;
+
+    static BaseList<BaseTexture*>* GetCharactersFromFont(BaseTexture* inTexture);
+
+    static BaseList<SpriteFontGlyph*>* GetCharacterDescriptors(BaseTexture* inTexture);
+
+private:
+    enum ProcessState
+    {
+        SEEKING,
+        BEGIN_CHAR,
+        LINE_SEEK,
+        PIXEL_FOUND,
+        END_CHAR,
+    };
+
+    static ProcessState State;
+
+};
+
