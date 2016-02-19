ab669fb6888c8cc5b1a801151b8593ecae01952b
Tristan Dube <tristan.dube@gmail.com>
2016-02-18 23:20:34 -0500

    Added test assets to check the new animation system and test code to check it out. Does not work yet,

--- src/Blackjack/main.cpp
+++ src/Blackjack/main.cpp
@@ -11,20 +11,41 @@
 #include <SDLUtilities.h>
 
 #include <StringList.h>
+#include <SpriteAnimation.h>
 
 int main()
 {
-    StringList* x = new StringList();
 
     GameEngine* eng = new GameEngine();
 
     eng->Init(800, 800);
 
-    //eng->Graphics->SetBackgroundColor(0x659CEFFF);
+    
     eng->Graphics->SetBackgroundColor(101, 156, 239);
 
     BlackjackGame* game = new BlackjackGame();
     eng->AttachActor(game);
+
+    ////////////////////////// TEST SECTION ///////////////////////
+    BaseSprite* animSprite = eng->CreateSprite("animu");
+    BaseTexture* one = eng->Graphics->CreateTexture("assets\\one.png");
+    BaseTexture* two = eng->Graphics->CreateTexture("assets\\two.png");
+    BaseTexture* three = eng->Graphics->CreateTexture("assets\\three.png");
+    BaseTexture* four = eng->Graphics->CreateTexture("assets\\four.png");
+
+    SpriteAnimation* anim = new SpriteAnimation();
+    anim->Frames->Add(one);
+    anim->Frames->Add(two);
+    anim->Frames->Add(three);
+    anim->Frames->Add(four);
+    animSprite->AddAnimation(anim);
+    animSprite->IsAnimated = true;
+
+    animSprite->SetPos(100, 100);
+    animSprite->Play();
+
+    //////////////////////// END TEST SECTION /////////////////////
+
 
     while (true)
     {
@@ -39,12 +60,6 @@ int main()
         {
             SaveTextAsBMP((SDLText*)game->gameDrawDialog->DialogText, "test.bmp");
         }
-
-        //if (eng->Keyboard->IsKeyClicked(Key::E))
-        //{
-        //    x->SpreadCardsOnScreen();
-        //}
-
     }
 
     return 0;
