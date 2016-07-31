8defbb0933c2739fd4a097a26b6351b9b37c3905
Tristan Dube <tristan.dube@gmail.com>
2016-07-31 13:06:07 -0400

    Added a math function to smooth out a curve by enlarging it by a factor.

--- src/CommsFramework/Functions.cpp
+++ src/CommsFramework/Functions.cpp
@@ -13,3 +13,36 @@ float* CreateSinusTrack(int amount, int amplitude)
 
     return ret;
 }
+
+float* SmoothCurveTrack(float* track, int trackLength, int factor)
+{
+    float* smoothedCurve = new float[trackLength * factor];
+
+    int k = 0;
+    for (int i = 0; i < trackLength; i++)
+    {
+        float nbOne = track[i];
+        float nbTwo = track[i + 1];
+
+        float diff = nbTwo - nbOne;
+        float increments = diff / factor;
+
+        smoothedCurve[k++] = nbOne;
+        for (int j = 1; j < factor; j++)
+        {
+            smoothedCurve[k++] = nbOne + (increments * j);
+        }
+    }
+
+    return smoothedCurve;
+}
+
+int avg(int one, int two)
+{
+    return (one + two) / 2;
+}
+
+float favg(float one, float two)
+{
+    return (one + two) / 2;
+}
--- src/CommsFramework/Functions.h
+++ src/CommsFramework/Functions.h
@@ -1,3 +1,8 @@
 #pragma once
 
 float* CreateSinusTrack(int amount, int amplitude);
+
+float* SmoothCurveTrack(float* track, int trackLength, int factor);
+
+int avg(int one, int two);
+float favg(float one, float two);
