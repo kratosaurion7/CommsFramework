90da4e087d14d494a047a4ebd710ab3fc0c78320
Tristan Dube <tristan.dube@gmail.com>
2016-07-30 23:38:30 -0400

    Test code to create tga with a plotted sinus t rack.

--- src/FantasyGame/main.cpp
+++ src/FantasyGame/main.cpp
@@ -13,10 +13,34 @@
 #include "FantasyGame.h"
 #include "LocalGrid.h"
 
+#include <TgaFile.h>
+#include <Functions.h>
+
 class Game_Start_Params;
 
 int main()
 {
+    int seqLen = 100;
+    float* seq = CreateSinusTrack(seqLen, 10);
+
+    TgaFile image = TgaFile();
+    image.Init(seqLen, seqLen);
+    
+    for (int i = 0; i < seqLen; i++)
+    {
+        int pt = seq[i];
+        int baseLine = i + (seqLen * seqLen / 2);
+        TgaPix* px = image.Pixels[baseLine + pt];
+        px->a = 255;
+        px->r = 255;
+        px->g = 0;
+        px->b = 255;
+    }
+
+    image.Save("out.tga");
+
+    return 0;
+
     FantasyGame* game = new FantasyGame();
     game->ReadConfig();
 
